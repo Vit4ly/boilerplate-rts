262c1843a2428c36eade00d93c7727bbb5fe34bf
Vitaliy <vit4ly.kiselev@gmail.com>
2022-11-18 21:50:13 +0300

    added storybook

---
+++ src/stories/IconBase/IconBase.stories.tsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import {ComponentStory, ComponentMeta} from '@storybook/react';
+// import IconBase from "../../components/Basic/IconBase/IconBase";
+//
+// export default {
+//     title: 'Example/Button',
+//     component: IconBase,
+// } as ComponentMeta<typeof IconBase>;
+//
+// const template: ComponentStory<typeof IconBase> = (args) => <IconBase {...args}/>;
+//
+// export const Simple = template.bind({})
+// Simple.args = {
+//     title: 'settings'
+// }
